8ed9874d3164c9014a8840ada6428f380d94417d
Sung Gon Yi <skonmeme@gmail.com>
2025-10-15 11:29:26 +0900

    example functions and networks

--- lib/nn/function.py
+++ lib/nn/function.py
@@ -3,7 +3,33 @@ from typing import Union
 import numpy as np
 
 
-class ReLU:
+class Function:
+    values: np.ndarray = None
+
+    def forward(self, x: Union[float, np.ndarray]) -> np.ndarray:
+        raise NotImplementedError
+
+    def backward(self) -> np.ndarray:
+        raise NotImplementedError
+
+
+class Constant(Function):
+    values: np.ndarray = None
+
+    def forward(self, x: Union[float, np.ndarray]) -> np.ndarray:
+        if isinstance(x, np.ndarray):
+            self.values = x
+        else:
+            self.values = np.array([x])
+        return self.values
+
+    def backward(self) -> np.ndarray:
+        if self.values is None:
+            raise ValueError("Constant has not been called yet")
+        return np.zeros_like(self.values)
+
+
+class ReLU(Function):
     values: np.ndarray = None
 
     def forward(self, x: Union[float, np.ndarray]) -> np.ndarray:
@@ -19,18 +45,18 @@ class ReLU:
         return np.where(self.values > 0, 1, 0)
 
 
-class Sigmoid:
-    values: np.ndarray = None
+class Sigmoid(Function):
+    output: np.ndarray = None
 
     def forward(self, x: Union[float, np.ndarray]) -> np.ndarray:
         if isinstance(x, np.ndarray):
-            self.values = x
+            values = x
         else:
-            self.values = np.array([x])
-        return 1 / (1 + np.exp(-self.values))
+            values = np.array([x])
+        self.output = 1 / (1 + np.exp(-values))
+        return self.output
 
     def backward(self) -> np.ndarray:
-        if self.derivatives is None:
+        if self.output is None
             raise ValueError("Sigmoid has not been called yet")
-        sigmoid = 1 / (1 + np.exp(-self.values))
-        return sigmoid * (1 - sigmoid)
+        return output * (1 - output)
--- lib/nn/network.py
+++ lib/nn/network.py
@@ -1,13 +1,53 @@
+from typing import List, Optional
+
+import numpy as np
+from function import Constant, Function, ReLU, Sigmoid
+
+
+class SimpleLayer:
+    def __init__(
+        self,
+        n_input,
+        n_output,
+        weights: Optional[np.ndarray] = None,
+        function: Function = Constant,
+    ):
+        self.weights = (
+            weights if weights is not None else np.random.randn(n_input, n_output)
+        )
+        self.function = function
+
+    def forward(self, inputs: np.ndarray) -> np.ndarray:
+        return self.function.forward(np.dot(inputs, self.weights))
+
+    def backward(self, gradients: np.ndarray) -> np.ndarray:
+        # fix me
+        return np.dot(gradients, self.function.backward())
+
+    def weights(self) -> np.ndarray:
+        return self.weights
+
+
 class SimpleNetwork:
-    def __init__(self, layers):
-        self.layers = layers
+    def __init__(self):
+        self.layers = []
+        self.layers.append(SimpleLayer(2, 2, np.array([0, 0, 0, 0]), ReLU()))
+        self.layers.append(SimpleLayer(2, 2, np.array([0, 0]), Sigmoid()))
 
-    def forward(self, inputs):
+    def _loss(self, expected: np.ndarray, actual: np.ndarray) -> float:
+        self.loss = np.sum((expected - actual) ** 2) / len(expected)
+        return self.loss
+
+    def forward(self, inputs: np.ndarray, outputs: np.ndarray) -> List[np.ndarray]:
+        layer_outputs = []
+        o = inputs
         for layer in self.layers:
-            inputs = layer.forward(inputs)
-        return inputs
+            o = layer.forward(o)
+            layer_outputs.append = o
+        layer_outputs.append(self._loss(outputs, o))
+        return layer_outputs
 
-    def backward(self, gradients):
+    def backward(self, gradients: np.ndarray) -> np.ndarray:
         for layer in reversed(self.layers):
             gradients = layer.backward(gradients)
         return gradients
