39b83671e5d292bd6d96092b0a0630b6e69efb45
Sung Gon Yi <skonmeme@gmail.com>
2025-10-17 08:20:21 +0900

    complete forward

--- src/backpropagation/__main__.py
+++ src/backpropagation/__main__.py
@@ -2,17 +2,20 @@ import numpy as np
 
 from backpropagation.network import SimpleNetwork
 
-w1 = np.array([[1.0, 0.5], [2.0, -0.5]])
-w2 = np.array([[0.5], [0.25]])
+w1 = np.array([[1.0, 0.5], [2.0, -1.0]])
+w2 = np.array([[0.5, 0.25]])
 
-x = np.array([[0.2, 0.3], [0.4, 0.5]])
-y = np.array([2, 2.1])
+x = np.array([[0.2, 0.3], [0.5, 0.5]])
+y = np.array([[0.5], [0.75]])
 
 
 def main():
-    network = SimpleNetwork()
+    network = SimpleNetwork(w1, w2)
     layer_outputs = network.forward(x, y)
     print("Layer outputs:", layer_outputs)
+    gradients, weights = network.backward()
+    print("Gradients:", gradients)
+    print("Weights:", weights)
 
 
 if __name__ == "__main__":
--- src/backpropagation/network.py
+++ src/backpropagation/network.py
@@ -13,40 +13,54 @@ class SimpleLayer:
         weights: Optional[np.ndarray] = None,
         function: Function = Constant,
     ):
-        self.weights = weights if weights is not None else np.random.randn(n_input, n_output)
+        self.weights = (
+            weights if weights is not None else np.random.randn(n_input, n_output)
+        )
         self.function = function
 
     def forward(self, inputs: np.ndarray) -> np.ndarray:
-        return self.function.forward(np.dot(self.weights, inputs))
+        self.inputs = inputs
+        self.outputs = self.function.forward(self.weights @ inputs)
+        return self.outputs
 
     def backward(self, gradients: np.ndarray) -> np.ndarray:
         # fix me
-        return np.dot(gradients, self.function.backward())
-
-    def weights(self) -> np.ndarray:
-        return self.weights
+        gradients = self.function.backward(gradients)
+        self.weights -= self.learning_rate * gradients @ self.inputs.T
+        return gradients @ self.weights.T
 
 
 class SimpleNetwork:
-    def __init__(self):
+    def __init__(
+        self, w1: np.ndarray, w2: np.ndarray, leanring_rate: np.float32 = 0.05
+    ):
         self.layers = []
-        self.layers.append(SimpleLayer(2, 2, np.array([[1.0, 0.5], [2, -0.5]]), ReLU()))
-        self.layers.append(SimpleLayer(2, 2, np.array([[0.5, 0.25]]), Sigmoid()))
+        self.layers.append(SimpleLayer(2, 2, w1, ReLU()))
+        self.layers.append(SimpleLayer(2, 1, w2, Sigmoid()))
+        self.learning_rate = leanring_rate
 
     def _loss(self, expected: np.ndarray, actual: np.ndarray) -> float:
-        self.loss = np.sum((expected - actual) ** 2) / len(expected)
+        self.loss = np.sum((expected.transpose() - actual) ** 2)
         return self.loss
 
     def forward(self, inputs: np.ndarray, outputs: np.ndarray) -> List[np.ndarray]:
+        self.inputs = inputs
+        self.outputs = outputs
         layer_outputs = []
         o = inputs
         for layer in self.layers:
             o = layer.forward(o)
             layer_outputs.append(o)
+            self.forwared = o
         layer_outputs.append(self._loss(outputs, o))
-        return layer_outputs
+        return [layer_outputs, self._loss(outputs, o)]
 
-    def backward(self, gradients: np.ndarray) -> np.ndarray:
+    def backward(self) -> (List[np.ndarray], List[np.ndarray]):
+        gradient = 2 * (self.forwared - self.outputs)
+        gradients = [gradient]
+        weights = []
         for layer in reversed(self.layers):
-            gradients = layer.backward(gradients)
-        return gradients
+            gradient = layer.backward(gradient)
+            gradients.append(gradient)
+            weights.append(layer.weights)
+        return (gradients, weights)
